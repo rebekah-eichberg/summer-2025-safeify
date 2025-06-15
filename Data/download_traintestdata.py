0554298fd819fe9cacc9257f7f0c773ded7505a2
almargo <alex@margolis.org>
2025-06-15 18:41:15 -0400

    Update download_traintestdata.py

--- Data/download_traintestdata.py
+++ Data/download_traintestdata.py
@@ -1,15 +1,19 @@
 import os
 from useful_functions import download_if_not_exists,verify_md5_file
 
+FILENAMES=["test_v1.parquet",
+           "train_v1.parquet",
+           "validation_v1.parquet",
+           "train_final_v1.parquet"]
+URLS=["https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EW-2lTPvnYJEjlI5pjlICCMB-Y3YOok_u8ZhOE2EC5esNQ?e=sbS8EX&download=1",
+      "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EcjEJGg0mUVAuLHIzzpDLRgBL5xj38zEurzpc_2dgr_g9A?e=CwyvCM&download=1",
+      "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EX9g9XsEwFhHvdx-moe2SJwByp6CLqavq8GhuSty33uenQ?e=wR4vEy&download=1",
+      "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EXSUALn38fBNmM-BSgp4GBMB5PDhFDsdQMAXs1z2JeNBIQ?e=fwdMZq&download=1"]
 
-URL_TEST="https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EW-2lTPvnYJEjlI5pjlICCMB-Y3YOok_u8ZhOE2EC5esNQ?e=sbS8EX&download=1"
-FILENAME_TEST="test_v1.parquet"
-URL_TRAIN="https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EcjEJGg0mUVAuLHIzzpDLRgBL5xj38zEurzpc_2dgr_g9A?e=CwyvCM&download=1"
-FILENAME_TRAIN="train_v1.parquet"
 
 
-download_if_not_exists(URL_TEST,FILENAME_TEST)
-download_if_not_exists(URL_TRAIN,FILENAME_TRAIN)
+for i in range(len(FILENAMES)):
+    download_if_not_exists(URLS[i],FILENAMES[i])
 
 
 verify_md5_file("md5_checksums.txt")
