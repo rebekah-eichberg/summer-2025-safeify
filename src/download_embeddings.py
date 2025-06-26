17a6e39515b99bd6f18b4ea5555dfb4d3987a629
almargo <alex@margolis.org>
2025-06-26 13:54:30 -0400

    Update embeddings download script

--- src/download_embeddings.py
+++ src/download_embeddings.py
@@ -7,3 +7,6 @@ URLS=[
     "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/EbkxqErVR05LqpZLDDeV-GUBx6o6QPPNFn4zTP8kP0s89Q?e=B0TCEI&download=1",
     "https://buckeyemailosu-my.sharepoint.com/:u:/g/personal/margolis_93_osu_edu/Ecds8B2Rl0lErYH2XiZQl30BqgW3AB-ZuyoeXzdBn99ecQ?e=S5gsh4&download=1"
     ]
+
+for i in range(len(FILENAMES)):
+    download_if_not_exists(URLS[i],FILENAMES[i])
